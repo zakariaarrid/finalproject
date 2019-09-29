a92739a2872dbdf3fa65779c71e8eb22e79eba20
zakaria <zakariaarrid0@gmail.com>
2019-09-29 23:24:07 +0100

    version stable 2

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,1 @@
-<?php
+@extends('layouts.admin')
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,19 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+/*mix.styles([
+    'resources/css/libs/blog-post.css',
+    'resources/css/libs/bootstrap.css',
+    'resources/css/libs/font-awesome.css',
+    'resources/css/libs/metisMenu.css',
+    'resources/css/libs/sb-admin-2.css',
+    'resources/css/libs/styles.css'
+
+],'public/css/app.css');*/
+mix.scripts([
+    'resources/js/jquery.js',
+    'resources/js/metisMenu.js',
+    'resources/js/sb-admin-2.js',
+    'resources/js/scripts.js',
+    'resources/js/bootstrap.js',
+],'public/js/app.js');
